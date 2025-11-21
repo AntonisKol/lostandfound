e0ffbf8c97a81fb1285e5a6d3e61b430e9298553
Matthieu Petitmangin <mpe@client3651.fritz.box>
2025-11-21 22:27:19 +0100

    header component

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View } from 'react-native';
+import Header from './components/Header';
+ 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <Header   />
+     </View>
   );
 }
 
@@ -13,7 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import { SafeAreaView, View, Text, Image, StyleSheet, ImageSourcePropType } from 'react-native';
+
+interface HeaderProps {
+  title?: string;
+  logo?: ImageSourcePropType;
+}
+
+export default function Header({ title = 'Fundstück', logo }: HeaderProps) {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+         <Text style={styles.title}>{title}</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: '#fff',
+  },
+  container: {
+    width: '100%',
+    height: '95%',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  
+  title: {
+    fontSize: 60,
+    },
+});
