90a8c590a31f100e3f9fe76a6f1293d224a6312b
Matthieu Petitmangin <mpe@client3651.fritz.box>
2025-11-21 21:52:47 +0100

    run app

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>LOST & FOUND</Text>
       <StatusBar style="auto" />
     </View>
   );
