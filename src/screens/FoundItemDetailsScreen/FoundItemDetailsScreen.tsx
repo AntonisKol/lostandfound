1f8e01904fbb7007137ca54bd99fbc68426d47fc
Antonis Kolovos <mpe@client3651.fritz.box>
2026-03-11 14:27:47 +0100

    satisfy linter

--- src/screens/FoundItemDetailsScreen/FoundItemDetailsScreen.tsx
+++ src/screens/FoundItemDetailsScreen/FoundItemDetailsScreen.tsx
@@ -1,8 +1,9 @@
- import { Text, Image, ScrollView, Button } from "react-native";
-import { useRoute, RouteProp } from "@react-navigation/native";
+import { Text, Image, ScrollView, Button } from "react-native";
+import { useRoute } from "@react-navigation/native";
 import { styles } from "./styled";
+import React from "react";
 
- type RootStackParamList = {
+type RootStackParamList = {
   FoundItemDetails: {
     id: string;
     image_url: string;
@@ -14,30 +15,33 @@ import { styles } from "./styled";
 };
 
 const FoundItemDetailsScreen = () => {
-  const route = useRoute<RouteProp<RootStackParamList, "FoundItemDetails">>();
-  const { image_url, category, location, notes } = route.params;
+  const route = useRoute();
+  const { image_url, category, location, notes } =
+    route.params as RootStackParamList["FoundItemDetails"];
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {image_url && <Image source={{ uri: image_url }} style={styles.image} />}
+
       <Text style={styles.label}>Category:</Text>
       <Text style={styles.text}>{category}</Text>
 
       <Text style={styles.label}>Location found:</Text>
       <Text style={styles.text}>{location}</Text>
 
-      {notes ? (
+      {notes && (
         <>
           <Text style={styles.label}>Notes:</Text>
           <Text style={styles.text}>{notes}</Text>
         </>
-      ) : null}
+      )}
 
-      <Button title="Claim / Contact Finder" onPress={() => alert("Feature coming soon!")} />
+      <Button
+        title="Claim / Contact Finder"
+        onPress={() => alert("Feature coming soon!")}
+      />
     </ScrollView>
   );
 };
-
-
 
 export default FoundItemDetailsScreen;
